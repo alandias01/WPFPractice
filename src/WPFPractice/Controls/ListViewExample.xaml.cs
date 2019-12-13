d8e3fed91de7ed1159a0268c0ecab74b03bbaf3e
alan <alandias01@yahoo.com>
2019-12-13 15:46:40 -0500

    added ListViewExample

---
+++ src/WPFPractice/Controls/ListViewExample.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFPractice.Controls
+{
+    /// <summary>
+    /// Interaction logic for ListViewExample.xaml
+    /// </summary>
+    public partial class ListViewExample : Window
+    {
+        public ObservableCollection<Employee> EmpList { get; set; }
+
+        public ListViewExample()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            this.EmpList = Employee.Load();
+        }
+    }
+}
