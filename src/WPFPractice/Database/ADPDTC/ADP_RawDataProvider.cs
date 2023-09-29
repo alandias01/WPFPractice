891ed533a7e16dac3ebd5d70dbcd63d7ebbc76ac
alan <alandias01@yahoo.com>
2023-09-29 10:44:16 -0400

    add files

---
+++ src/WPFPractice/Database/ADPDTC/ADP_RawDataProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ADPDTC.Data
+{
+    public class ADP_RawDataProvider : IDataProvider<ADP_Raw>
+    {
+        ADPDTCEntities AE = new ADPDTCEntities();
+                
+        public void Load(out ADP_Raw obj, int Id)
+        {
+            obj = AE.ADP_Raw.FirstOrDefault(x => x.Tran_Id == Id);
+        }
+
+        public void Load(ICollection<ADP_Raw> collection)
+        {            
+            AE.ADP_Raw.ToList().ForEach(collection.Add);
+        }
+
+        public void Load(ICollection<ADP_Raw> collection, DateTime dateOfData)
+        {
+            AE.ADP_Raw.Where(x => x.Process_Date == dateOfData).ToList().ForEach(collection.Add);
+        }
+        
+        public void Insert(ADP_Raw obj)
+        {
+            AE.ADP_Raw.AddObject(obj);
+        }
+
+        public void Update(ADP_Raw obj)
+        {
+            if (AE.ADP_Raw.Any(x => x.Tran_Id == obj.Tran_Id))
+            {
+                AE.SaveChanges();
+            }
+        }
+
+        public void Delete(ADP_Raw obj)
+        {
+            AE.ADP_Raw.DeleteObject(obj);
+        }
+
+        
+    }
+}
---
+++ src/WPFPractice/Database/ADPDTC/PARTPODataProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADPDTC.Data
+{
+    public class PARTPODataProvider
+    {
+        ADPDTCEntities AE = new ADPDTCEntities();
+
+        public void Load(out PARTPO obj, int Id)
+        {
+            obj = AE.PARTPOes.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public void Load(ICollection<PARTPO> collection)
+        {
+            AE.PARTPOes.ToList().ForEach(collection.Add);
+
+        }
+
+        public void Load(ICollection<PARTPO> collection, DateTime dateOfData)
+        {
+            AE.PARTPOes.Where(x => x.DateofData == dateOfData).ToList().ForEach(collection.Add);
+        }
+
+        public void Insert(PARTPO obj)
+        {
+            AE.PARTPOes.AddObject(obj);
+            AE.SaveChanges();
+        }
+
+        public void Update(PARTPO obj)
+        {
+            if (AE.PARTPOes.Any(x => x.Id == obj.Id))
+            {
+                AE.SaveChanges();
+            }
+        }
+
+        public void Delete(PARTPO obj)
+        {
+            AE.PARTPOes.DeleteObject(obj);
+        }
+    }
+}
